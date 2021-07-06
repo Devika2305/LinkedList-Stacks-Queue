130fc359282346eef9eafdc20d13592b3e31065f
Devika Gupta <57962316+Devika2305@users.noreply.github.com>
2021-07-06 11:26:19 +0530

    Update MiddleElement.java

--- Stacks/src/MiddleElement.java
+++ Stacks/src/MiddleElement.java
@@ -1,50 +1,47 @@
+import java.util.Stack;
 
 //https://www.geeksforgeeks.org/design-a-stack-with-find-middle-operation/
 public class MiddleElement {
 
-	Node head = null;
-	int count=0;
-	
-	void push(int data) {
-		Node toAdd = new Node(data);
-		toAdd.prev = null;
-		toAdd.next = head;
-		count++;
-		
-		//if(count == 1)
-			//st.mid = toAdd;
-		
-	
-//		Node temp=head;
-//		while(temp.next!=null) {
-//			temp=temp.next;
-//		}
-//		temp.next=toAdd;
-//		count++;
-	}
-	
-	static class Stack{
-		Stack st = new Stack();
-		Node head;
-		Node mid;
-		int count;
-	}
-	static class Node{
-		Node prev;
-		Node next;
-		int data;
-		
-		Node(int data){
-			prev = next = null;
-			this.data = data;
+	private static int mid(Stack<Integer> st, int size) {
+
+		if(size == 0) {
+			
+			return 0;
 		}
+		int k = (size/2)+1;
+		int a = solve(st,k);
+		return a;
+	}
+	
+	private static int solve(Stack<Integer> st, int k) {
+
+		if(k == 1) {
+			return k;
+		}
+		int temp = st.peek();
+		st.pop();
+		solve(st,k-1);
+		st.push(temp);
+		return k;
 		
 	}
+
 	public static void main(String[] args) {
 
-		Stack st = new Stack();
-	
-		MiddleElement obj = new MiddleElement();
-	}
+		Stack<Integer> st = new Stack<Integer>();
+		st.push(1);
+		st.push(2);
+		st.push(3);
+		st.push(4);
+		st.push(5);
+		st.push(6);
+		int size = st.size();
+		System.out.println("Stack ");
+		System.out.println(st);
+		int middle = mid(st,size);
+		System.out.println("Middle Element = "+middle);
+		
+	}	
 
 }
